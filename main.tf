da9bb189d02b92f9c879dd9a9aa655083750cc5c
Diogo Barros <diogo.ma.barros03@gmail.com>
2024-09-28 19:28:17 +0100

    feat(ecr): create new infrastructure provisioning (Elastic Container Registery

--- main.tf
+++ main.tf
@@ -5,3 +5,7 @@ module "iam_user_and_role" {
 module "security_group"{
   source = "./modules/security_group"
 }
+
+module "ecr"{
+  source = "./modules/ecr"
+}
---
+++ modules/ecr/output.tf
@@ -0,0 +1,3 @@
+output "ecr_repository_url"{
+    value=aws_ecr_repository.my-cloud-app.repository_url
+}
