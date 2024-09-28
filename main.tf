65ac01e06b2553fba86020fe0221fb5584798cae
Diogo Barros <diogo.ma.barros03@gmail.com>
2024-09-28 20:10:38 +0100

    feat(terraform): add S3 backend configuration for state management
    - Configured S3 bucket for storing Terraform state files.
    - Set up  as the path for the state file.
    - Enabled remote state management to prevent local state handling.

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "050451375189-state-bucket"
+    key    = "aws-infra/terraform.tfstate"
+    region = "us-west-1"
+  }
+}
+
 module "iam_user_and_role" {
   source = "./modules/iam_user_and_role"
 }
@@ -9,3 +17,4 @@ module "security_group"{
 module "ecr"{
   source = "./modules/ecr"
 }
+
