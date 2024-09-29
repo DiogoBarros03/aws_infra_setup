ab322ab29ae22b4e6c86283972254a884987d5b8
Diogo Barros <diogo.ma.barros03@gmail.com>
2024-09-29 14:34:53 +0100

    feat(eks): update EKS configuration to use existing IAM role and VPC resources
    - Changed to use existing IAM role for EKS cluster (eks-cluster-role) via data source instead of creating new IAM policies.
    - Updated VPC configuration to use default VPC and associated subnets with aws_subnets data source.
    - Added security group reference using an existing security group ID.
    - Applied consistent naming for EKS addons (vpc-cni, coredns, kube-proxy).
    - Removed unnecessary aws_iam_role_policy_attachment references and dependencies.

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,16 +1,16 @@
-data "aws_vpc" "default-vpc"{
-    default=true
+data "aws_vpc" "default" {
+  default = true
 }
 
 data "aws_subnets" "default_subnets" {
   filter {
-    name   = data.aws_vpc.default-vpc.tags["Name"]
+    name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
 
 data "aws_security_group" "custom_sg" {
-  id = "sg-028f8637d0284fad8" 
+  id = "sg-028f8637d0284fad8"
 }
 
 data "aws_iam_role" "iam_eks_role" {
@@ -19,34 +19,28 @@ data "aws_iam_role" "iam_eks_role" {
 
 resource "aws_eks_cluster" "app_eks_cluster" {
   name     = "cloud-native-cluster"
-  role_arn = aws_iam_role.iam_eks_role.arn
+  role_arn = data.aws_iam_role.iam_eks_role.arn
 
-  
   vpc_config {
-    subnet_ids         = data.aws_subnet_ids.default_subnets.ids
+    subnet_ids         = data.aws_subnets.default_subnets.ids
     security_group_ids = [data.aws_security_group.custom_sg.id]
-  } 
-
-  depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController,
-  ]
+  }
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.app_eks_cluster.name
-  addon_name   = "vpc-cni"
+  cluster_name      = aws_eks_cluster.app_eks_cluster.name
+  addon_name        = "vpc-cni"
   resolve_conflicts = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.app_eks_cluster.name
-  addon_name   = "coredns"
+  cluster_name      = aws_eks_cluster.app_eks_cluster.name
+  addon_name        = "coredns"
   resolve_conflicts = "OVERWRITE"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.app_eks_cluster.name
-  addon_name   = "kube-proxy"
+  cluster_name      = aws_eks_cluster.app_eks_cluster.name
+  addon_name        = "kube-proxy"
   resolve_conflicts = "OVERWRITE"
 }
