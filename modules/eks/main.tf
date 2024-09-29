fe7cf200a17408a632dc6a476e761618a15ea646
Diogo Barros <diogo.ma.barros03@gmail.com>
2024-09-29 14:36:45 +0100

    refactor(eks): replace deprecated resolve_conflicts with new attributes
    - Replaced deprecated resolve_conflicts with resolve_conflicts_on_create and resolve_conflicts_on_update in aws_eks_addon resources.
    - Ensured proper conflict resolution during creation and update of EKS add-ons (vpc-cni, coredns, kube-proxy).

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -28,19 +28,25 @@ resource "aws_eks_cluster" "app_eks_cluster" {
 }
 
 resource "aws_eks_addon" "vpc_cni" {
-  cluster_name      = aws_eks_cluster.app_eks_cluster.name
-  addon_name        = "vpc-cni"
-  resolve_conflicts = "OVERWRITE"
+  cluster_name = aws_eks_cluster.app_eks_cluster.name
+  addon_name   = "vpc-cni"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
 }
 
 resource "aws_eks_addon" "coredns" {
-  cluster_name      = aws_eks_cluster.app_eks_cluster.name
-  addon_name        = "coredns"
-  resolve_conflicts = "OVERWRITE"
+  cluster_name = aws_eks_cluster.app_eks_cluster.name
+  addon_name   = "coredns"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
-  cluster_name      = aws_eks_cluster.app_eks_cluster.name
-  addon_name        = "kube-proxy"
-  resolve_conflicts = "OVERWRITE"
+  cluster_name = aws_eks_cluster.app_eks_cluster.name
+  addon_name   = "kube-proxy"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
 }
